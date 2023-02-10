d9354c76fe433967daeccedfbdb136655126798a
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-02-10 19:44:47 +0000

    commit

--- src/examples/02_expressions/01_expressions/expressions.h
+++ src/examples/02_expressions/01_expressions/expressions.h
@@ -3,4 +3,3 @@ Write function prototype operator_precedence_1 with three int pa
 /*
 Write function prototype operator_precedence_2 with three int parameters
 */
-int operator
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,47 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include<iostream>
 
 //write namespace using statement for cout
-
+using std::cout; 
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	double meal_amount = 0;
+	double tip_rate = 0;
+	double tip_amount = 0;
+	double tax_amount = 0;
+	double total = 0;
+
+	cout<<"Enter cost of the meal: ";
+	cin>> meal_amount;
+
+	get_sales_tax_amount(meal_amount);
+	{
+		tax_amount = get_sales_tax_amount(meal_amount);
+	}
 	
+	
+
+	cout<<"Enter the tip rate desired: ";
+	cin>>tip_rate;
+
+	get_tip_amount(meal_amount, tip_rate);
+	{
+		tip_amount = get_tip_amount(meal_amount, tip_rate);
+	}
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"\n";
+	cout<<"Meal Amount: $"<<(meal_amount)<<"\n";
+	cout<<"Sales Tax: $"<<(tax_amount)<<"\n";
+	cout<<"Tip Amount: $"<<(tip_amount)<<"\n";
+	cout<<"Total: $"<<(total)<<"\n";
 
 	return 0;
 }
