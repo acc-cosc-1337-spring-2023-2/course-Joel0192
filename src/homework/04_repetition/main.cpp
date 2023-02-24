e2e81a5a1508d13a6c9ad83dce1b764228802e0c
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-02-24 23:45:09 +0000

    Hw#4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,16 +1,66 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
+using std::cout;
+using std::cin;
+using std::string;
 
-
-/*
-Write code that prompts user to enter 1 for Get GC Content, 
-or 2 for Get DNA Complement.  The program will prompt user for a 
-DNA string and call either get gc content or get dna complement
-function and display the result. Program runs as long as 
-user enters a y or Y.
-*/
 int main() 
 {
+	auto choice = 0;
+	string option;
+	auto integer = 0;
+	auto integer2 = 0;
+	auto result = 0;
+	do
+	{
+		cout<<"1-Factorial"<<"\n";
+		cout<<"2-Greatest Common Divisor"<<"\n";
+		cout<<"3-Exit"<<"\n";
+		cout<<"Please enter an option: ";
+		cin>>choice;
+		cout<<"\n";
+		if(choice == 1)
+		{
+			cout<<"Enter a positive integer: ";
+			cin>>integer;
+			result = factorial(integer);
+			cout<<"The Factorial of the number is: "<<result<<"\n\n";
+		}
+		else if(choice == 2)
+		{
+			cout<<"Enter two positive integers"<<"\n";
+			cout<<"#1: ";
+			cin>>integer;
+			cout<<"#2: ";
+			cin>>integer2;
+			cout<<"\n";
+			result = gcd(integer, integer2);
+			cout<<"The Greatest common divisor of these numbers is: "<<result<<"\n\n";
+		}
+		else if(choice == 3)
+		{
+			cout<<"are you sure you want to exit?"<<"\n";
+			cout<<"Enter y or n: ";
+			cin>>option;
+			if(option == "y"||option == "Y")
+			{
+				choice += -4;
+				cout<<"Thank You!"<<"\n";
+			}
+			else
+			{
+				cout<<"\n";
+			}
+			
+		
+		}
+		else
+		{
+			cout<<"Invalid option, Try again"<<"\n\n";
+		}
+	} while (choice != -1);
+	
 	return 0;
 }
