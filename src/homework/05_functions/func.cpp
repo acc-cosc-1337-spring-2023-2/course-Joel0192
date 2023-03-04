35f12a89bdad8975048829ca34595d48dc234882
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-03-04 22:02:41 +0000

    HW5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,77 @@
 //add include statements
-
+#include "func.h"
+#include <iostream>
+#include <cctype>
+using std::string;
 //add function code here
+
+double get_gc_content(const string &dna)
+{
+    double content_count = 0;
+
+    for(auto &a : dna)
+    {
+        if(a == 'G' || a == 'C')
+        {
+            content_count++;
+        }
+
+    }
+    return content_count / dna.length();
+}
+
+string reverse_string(string dna)
+{
+    string result;
+    for(auto b = dna.length(); b > 0; b--)
+    {
+        result.push_back(dna[b - 1]);
+    }
+    return result;
+}
+
+string get_dna_compliment(string dna)
+{
+    string dna_compliment = reverse_string(dna);
+    for(auto c = 0; c < dna_compliment.length(); c++)
+    {
+        auto d = dna_compliment[c];
+        if(d == 'A')
+        {
+            dna_compliment[c] = 'T';
+        }
+        else if(d == 'T')
+        {
+            dna_compliment[c] = 'A';
+        }
+        else if(d == 'G')
+        {
+            dna_compliment[c] = 'C';
+        }
+        else if(d == 'C')
+        {
+            dna_compliment[c] = 'G';
+        }
+
+    }
+    return dna_compliment;
+}
+
+bool valid_input(string dna) 
+{
+    bool valid;
+    for (auto f = 0; f < dna.length(); f++)
+    {
+        auto e = dna[f];
+        if(e == 'C' || e == 'G' || e == 'T' || e == 'A') 
+        {
+            valid = true;
+        }
+        else 
+        {
+            valid = false;
+            break;
+        }
+    }
+    return valid;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,77 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
+#include<iostream>
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
+using std::string;
+using std::cout;
+using std::cin;
 
-*/
 int main() 
 {
+	auto choice = 0;	
+	string dna;
+	string option;
 
+	do
+	{
+		cout<<"1-Get GC Content"<<"\n";
+		cout<<"2-Get DNA Complement"<<"\n";
+		cout<<"3-Exit"<<"\n";
+		cout<<"Please enter an option: ";
+		cin>>choice;
+		cout<<"\n";
+		if(choice == 1)
+		{
+			
+			cout<<"Enter a DNA string: ";
+			cin>>dna;
+			if(valid_input(dna))
+			{
+				cout<<"The GC content is "<<get_gc_content(dna)*100<<"%"<<"\n\n";
+			}
+			else
+			{
+				cout<<"Invalid option."<<"\n\n";
+			}
+		}
+		else if(choice == 2)
+		{
+			cout<<"Enter a DNA string: ";
+			cin>>dna;
+			if(valid_input(dna))
+			{
+				cout<<"The dna compliment is "<<get_dna_compliment(dna)<<"\n\n";
+			}
+			else
+			{
+				cout<<"Invalid option."<<"\n\n";
+			}
+			cout<<"\n";
+			
+		}
+		else if(choice == 3)
+		{
+			cout<<"are you sure you want to exit?"<<"\n";
+			cout<<"Enter y or n: ";
+			cin>>option;
+			if(option == "y"||option == "Y")
+			{
+				
+				cout<<"Thank You!"<<"\n";
+				
+			}
+			else
+			{
+				cout<<"\n";
+			}
+			
+		
+		}
+		else
+		{
+			cout<<"Invalid option, Try again"<<"\n\n";
+		}
+	} while (option != "y" && option != "Y");
+	
+	
 	return 0;
 }
