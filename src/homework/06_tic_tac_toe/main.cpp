5866650d565898a5c5a82137a021be8cf179bdcc
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-03-24 22:29:22 +0000

    hw6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,42 @@
+#include <iostream>
+#include "tic_tac_toe.h"
+using std::string;
+using std::cout;
+using std::cin;
 
-int main() 
+int main()
 {
+    string first_player;
+    cout << "Enter first player (X or O): ";
+    cin >> first_player;
 
-	return 0;
+    TicTacToe play;
+    play.start_game(first_player);
+
+    bool continue_play = true;
+    while (continue_play)
+    {
+        int position;
+        cout << "Player " << play.get_player() << ", enter a position (1-9): ";
+        cin >> position;
+        play.mark_board(position);
+
+        play.display_board();
+        if (play.game_over())
+        {
+            cout << "Game over!\n";
+            continue_play = false;
+        }
+    }
+
+    cout << "Would you like to play again? (Y/N): ";
+    char play_again;
+    cin >> play_again;
+    if (play_again == 'Y' || play_again == 'y')
+    {
+        play.clear_board();
+        main();
+    }
+
+    return 0;
 }
